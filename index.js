c86b425467e61872f23ca387a2e7a7c4c67d5953
Sublicuentas <marvinvalenzuela293@gmail.com>
2026-02-21 13:14:15 -0600

    Add HTTP server to respond with 'OK'

--- index.js
+++ index.js
@@ -131,3 +131,13 @@ http.createServer((req, res) => {
 }).listen(PORT, () => {
   console.log("🌐 Web service activo en puerto " + PORT);
 });
+
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK");
+}).listen(PORT, "0.0.0.0", () => {
+  console.log("🌐 Listening on " + PORT);
+});
