75b016516ad7b40a5fc346db17839acae606a554
Sublicuentas <marvinvalenzuela293@gmail.com>
2026-03-23 19:39:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { bootCore } = require("./index_01_core");
+const { startBotPollingSafe } = require("./index_01_core");
 
 require("./index_02_utils_roles");
 require("./index_03_clientes_crm");
@@ -6,9 +6,6 @@ require("./index_04_inventario_correos");
 require("./index_05_finanzas_menus");
 require("./index_06_handlers");
 
-bootCore()
-  .then(() => console.log("✅ Bot iniciado"))
-  .catch((e) => {
-    console.error("❌ Error al iniciar:", e?.message || e);
-    process.exit(1);
-  });
+(async () => {
+  await startBotPollingSafe();
+})();
